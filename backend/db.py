3a54c69a3f5c6c16ea22cc53770a5a3ba3a29b04
eco-NIN <2321987772@qq.com>
2025-05-13 16:07:17 +0800

    完善架构目录

---
+++ backend/db.py
@@ -0,0 +1,3 @@
+# @Author  : eco
+# @Date    ：2025/5/13 16:03
+# @Function: MySQL数据库连接管理
---
+++ backend/main.py
@@ -0,0 +1,19 @@
+# @Author  : eco
+# @Date    ：2025/5/13 15:57
+# @Function: 应用主入口
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
